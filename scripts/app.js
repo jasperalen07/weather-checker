bf1eb247f7819a9ea199c32be2e7427f9553bd69
jasperalen07 <125176411+jasperalen07@users.noreply.github.com>
2024-06-03 16:51:39 -0600

    created an event listener

--- scripts/app.js
+++ scripts/app.js
@@ -6,3 +6,16 @@ const humid = document.getElementById('humid')
 const description = document.getElementById('description')
 const weatherIcon = document.getElementById('weatherIcon')
 const weatherInfo = document.getElementById('weatherInfo')
+
+//Do a Event Listener for the button
+searchInput.addEventListener('click', function(){
+    const city = cityInput.value;
+    if(city){
+        fetchWeatherData(city);
+    } else{
+        alert('Please enter a city name')
+    }
+   
+});
+
+//Function to fetch the Api
