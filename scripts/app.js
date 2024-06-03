66c6b9d90cd264c4f4b0eea14a5bddfa64339593
jasperalen07 <125176411+jasperalen07@users.noreply.github.com>
2024-06-03 16:44:37 -0600

    started declaring elements

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+// declare variables for the ID's
+const searchInput = document.getElementById('searchInput')
+const cityInput = document.getElementById('city')
+const temperature = document.getElementById('temperature')
+const humid = document.getElementById('humid')
+const description = document.getElementById('description')
+const weatherIcon = document.getElementById('weatherIcon')
+const weatherInfo = document.getElementById('weatherInfo')
