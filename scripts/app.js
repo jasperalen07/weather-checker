b1d71384e7aa8eaa2fa9b766f9e9d7ae7acb4046
jasperalen07 <125176411+jasperalen07@users.noreply.github.com>
2024-06-03 18:29:16 -0600

    finishing up the function

--- scripts/app.js
+++ scripts/app.js
@@ -19,3 +19,30 @@ searchInput.addEventListener('click', function(){
 });
 
 //Function to fetch the Api
+async function fetchWeatherData(city){
+const apiKey = 'af4f65ffb755d5558f112052eaa97dbf';
+const apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+try {
+    const response = await fetch(apiUrl);
+    if(!response.ok){
+        throw new Error('City is not Found');
+    }
+    const data = await response.json();
+    displayWeatherData(data);
+} catch(error){
+    alert(error.message);
+}
+
+}
+
+// Function for the weather data 
+
+function displayWeatherData(data) {
+    cityInput.textContent = data.name;
+    temperature.textContent = `Temperature: ${data.main.temp}C`;
+    humid.textContent = `Humidity:  ${data.main.humid}%`;
+    description.textContent = data.weather[0].description;
+    weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png;`
+   weatherInfo.classList.remove('hidden');
+}
